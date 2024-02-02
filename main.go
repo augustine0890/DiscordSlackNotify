90e69b5e3807d3136a55f0f998942213df0cb3a6
augustine <augustino0890@gmail.com>
2024-02-02 12:33:54 +0900

    send the message to the slack channel

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"discord-slack-notify/util"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
+)
 
 func main() {
+	stage := flag.String("stage", "prod", "The environment running")
+	flag.Parse()
+
+	// Loading environment variables
+	err := util.LoadEnv(*stage)
+	if err != nil {
+		fmt.Printf("Error loading environmnet variables: %v\n", err)
+	}
+	log.Printf("Running with %v environment\n", *stage)
+
 	fmt.Println("DiscordSlackNotify")
+
+	runSlack()
+}
+
+func runSlack() {
+	SLACK_AUTH_TOKEN := os.Getenv("SLACK_AUTH_TOKEN")
+	CHANNEL_ID := "C06H3GZNG00" // #discord-noti channel
+
+	api := slack.New(SLACK_AUTH_TOKEN)
+	attachment := slack.Attachment{
+		Pretext: "For testing purpose",
+		Text:    "Have a good day!",
+	}
+	channelID, timestamp, err := api.PostMessage(
+		CHANNEL_ID,
+		slack.MsgOptionText("Main Message", false),
+		slack.MsgOptionAttachments(attachment),
+	)
+	if err != nil {
+		log.Fatalf("Error posting message: %s\n", err)
+	}
+
+	log.Printf("Message successfully sent to Channel %s at %s\n", channelID, timestamp)
 }
